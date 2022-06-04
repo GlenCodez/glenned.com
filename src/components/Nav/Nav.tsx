45f72b5ac76b0a0d9c4923fbc75c10608debc3cd
Glen <G@glenned.com>
2022-06-04 19:18:49 -0400

    Profile picture

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,6 +5,9 @@ import {Link} from "react-router-dom";
 function Nav() {
     return (
         <nav className="main-nav">
+            <div className="profile-container">
+                <img src="img/glen_burchfield_profile_picture.jpg" alt="Glen Burchfield Profile Picture" />
+            </div>
             <Link to="/home">Home</Link>
             <Link to="/pilot">Pilot</Link>
         </nav>
