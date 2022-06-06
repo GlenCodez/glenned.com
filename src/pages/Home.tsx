281a12b862937610df8594c8369a293a75727efb
Glen <G@glenned.com>
2022-06-05 22:40:29 -0400

    Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 function Home() {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1 className="home-h1">Hello</h1>
+            <h3 className="home-h3">I'm Glen</h3>
         </div>
     )
 }
