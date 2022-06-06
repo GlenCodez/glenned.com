407c5fc1afcc5a321b41b0482f0dc58c188e05c5
Glen <G@glenned.com>
2022-06-05 20:44:39 -0400

    Navigation header WIP

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,11 +5,18 @@ import {Link} from "react-router-dom";
 function Nav() {
     return (
         <nav className="main-nav">
-            <div className="profile-container">
-                <img src="img/glen_burchfield_profile_picture.jpg" alt="Glen Burchfield Profile Picture" />
+            <div className="nav-section">
+                <div className="profile-container">
+                    <img src="img/glen_burchfield_profile_picture.jpg" alt="Glen Burchfield Profile Picture" />
+                </div>
             </div>
-            <Link to="/home">Home</Link>
-            <Link to="/pilot">Pilot</Link>
+            <div className="nav-section">
+                <Link to="/home">Home</Link>
+                <Link to="/pilot">Pilot</Link>
+            </div>
+            <div className="nav-section">
+
+            </div>
         </nav>
     );
 }
