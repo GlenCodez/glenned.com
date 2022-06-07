70f9ea03855be6037c7c1e50ccb201b6ec4c9326
Glen <G@glenned.com>
2022-06-06 21:09:23 -0400

    Landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,18 @@ import React from 'react';
 function Home() {
     return (
         <div>
-            <h1 className="home-h1">Hello</h1>
-            <h3 className="home-h3">I'm Glen</h3>
+            <section className="landing">
+                <div className="landing-left">
+
+                </div>
+                <div className="landing-right">
+                    <h1 className="home-h1">Hello</h1>
+                    <h3 className="home-h3">I'm Glen</h3>
+                </div>
+            </section>
+            <section className="timeline">
+                <h1>Timeline.</h1>
+            </section>
         </div>
     )
 }
