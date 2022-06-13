04337eecdc1dbc67eb142bdfd67828638214d92e
Glen <G@glenned.com>
2022-06-13 18:00:52 -0400

    style typewriter

--- src/components/Typewriter/Typewriter.tsx
+++ src/components/Typewriter/Typewriter.tsx
@@ -5,52 +5,27 @@ type TypewriterProps = {
     comment?: string
     data: {
         [key:string]: string
-    }
+    },
+    name: string
 }
 
-function Typewriter({comment, data}:TypewriterProps){
+function Typewriter({comment, data, name}:TypewriterProps){
 
     return (
         <div className="typewriter">
-            <div id="sppb-addon-wrapper-1571746725182" className="sppb-addon-wrapper  sppb-wow fadeIn  sppb-animated" style={{ visibility: "visible", animationName: "fadeIn" }}>
-                <div id="sppb-addon-1571746725182" className=" sppb-wow fadeIn clearfix  sppb-animated"
-                     data-sppb-wow-duration="600ms" data-sppb-wow-delay="500ms"
-                     style={{visibility: "visible", animationDuration: "600ms", animationDelay: "500ms", animationName: "fadeIn" }}>
-                    <div className="sppb-addon sppb-addon-text-block  ">
-                        <div className="sppb-addon-content" style={{height: "120px"}}>
-                            <div id="typed-string" style={{display: "none"}}>
-                                <code>
-                                    {comment && <><span className="code_comment">// {comment}</span><br/></>}
-                                    <span className="code_selector">Name</span>
-                                    <span className="code_braces">&#123;</span><br/>
-                                    {
-                                        Object.entries(data).map(line => {
-                                            const [key,value] = line
-                                            return (
-                                                <>&nbsp; &nbsp;<span className="code_property">{key}:</span><span className="code_value">{value};</span> <br/></>
-                                            )
-                                        })
-                                    }
-                                    <span className="code_braces">&#125;</span>
-                                </code>
-                            </div>
-                            <code id="typed">
-                                {comment && <><span className="code_comment">// {comment}</span><br/></>}
-                                <span className="code_selector">Name</span> <span className="code_braces">&#123;</span><br/>
-                                {
-                                    Object.entries(data).map(line => {
-                                        const [key,value] = line
-                                        return (
-                                            <>&nbsp; &nbsp;<span className="code_property">{key}:</span><span className="code_value">{value};</span> <br/></>
-                                        )
-                                    })
-                                }
-                                <span className="code_braces">&#125;</span>
-                            </code><span className="typed-cursor typed-cursor--blink">|</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <code id="typed">
+                        {comment && <><span className="code_comment">// {comment}</span><br/></>}
+                        <span className="code_selector">{name} = </span> <span className="code_braces">&#123;</span><br/>
+                        {
+                            Object.entries(data).map(line => {
+                                const [key,value] = line
+                                return (
+                                    <>&nbsp; &nbsp;<span className="code_property">{key}:</span><span className="code_value">{value};</span> <br/></>
+                                )
+                            })
+                        }
+                        <span className="code_braces">&#125;</span>
+        </code><span className="typed-cursor typed-cursor--blink">|</span>
         </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,8 +11,12 @@ function Home() {
                 <div className="landing-right">
                     <h1 className="home-h1">Hello</h1>
                     <h3 className="home-h3">I'm Glen</h3>
-                    <Typewriter data={{First: "Glen", Last: "Burchfield"}} />
-                    <Typewriter data={{Profession: "Software Engineer", Expert: "Node.js"}} />
+                    <div className="lr-box1">
+                        <Typewriter data={{First: "Glen", Last: "Burchfield"}} name="Name" />
+                    </div>
+                    <div className="lr-box2">
+                        <Typewriter data={{Profession: "Software Engineer", Expert: "Node.js"}} name="Profile" />
+                    </div>
                 </div>
             </section>
             <section className="timeline">
