46b322ad235df9e20747ac6075af55c29489e756
Glen <G@glenned.com>
2022-06-12 20:13:59 -0400

    WIP Typewriter

--- src/components/Typewriter/Typewriter.tsx
+++ src/components/Typewriter/Typewriter.tsx
@@ -2,25 +2,54 @@ import './Typewriter.css';
 import {useEffect, useRef} from "react";
 
 type TypewriterProps = {
-    data: string
+    comment?: string
+    data: {
+        [key:string]: string
+    }
 }
 
-function Typewriter({data}:TypewriterProps){
-    const typewriterDataRef = useRef(null)
-    const containers = [
-        (<div className="typewriter-block"></div>)
-    ];
-
-    useEffect(() => {
-        const typewriterElement = typewriterDataRef.current;
-        console.log(typewriterElement)
-    }, [])
+function Typewriter({comment, data}:TypewriterProps){
 
     return (
         <div className="typewriter">
-            <div className="typewriter-data" ref={typewriterDataRef}>
-                <h3>Typewriter</h3>
-                <p>{data}</p>
+            <div id="sppb-addon-wrapper-1571746725182" className="sppb-addon-wrapper  sppb-wow fadeIn  sppb-animated" style={{ visibility: "visible", animationName: "fadeIn" }}>
+                <div id="sppb-addon-1571746725182" className=" sppb-wow fadeIn clearfix  sppb-animated"
+                     data-sppb-wow-duration="600ms" data-sppb-wow-delay="500ms"
+                     style={{visibility: "visible", animationDuration: "600ms", animationDelay: "500ms", animationName: "fadeIn" }}>
+                    <div className="sppb-addon sppb-addon-text-block  ">
+                        <div className="sppb-addon-content" style={{height: "120px"}}>
+                            <div id="typed-string" style={{display: "none"}}>
+                                <code>
+                                    {comment && <><span className="code_comment">// {comment}</span><br/></>}
+                                    <span className="code_selector">Name</span>
+                                    <span className="code_braces">&#123;</span><br/>
+                                    {
+                                        Object.entries(data).map(line => {
+                                            const [key,value] = line
+                                            return (
+                                                <>&nbsp; &nbsp;<span className="code_property">{key}:</span><span className="code_value">{value};</span> <br/></>
+                                            )
+                                        })
+                                    }
+                                    <span className="code_braces">&#125;</span>
+                                </code>
+                            </div>
+                            <code id="typed">
+                                {comment && <><span className="code_comment">// {comment}</span><br/></>}
+                                <span className="code_selector">Name</span> <span className="code_braces">&#123;</span><br/>
+                                {
+                                    Object.entries(data).map(line => {
+                                        const [key,value] = line
+                                        return (
+                                            <>&nbsp; &nbsp;<span className="code_property">{key}:</span><span className="code_value">{value};</span> <br/></>
+                                        )
+                                    })
+                                }
+                                <span className="code_braces">&#125;</span>
+                            </code><span className="typed-cursor typed-cursor--blink">|</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,8 +11,8 @@ function Home() {
                 <div className="landing-right">
                     <h1 className="home-h1">Hello</h1>
                     <h3 className="home-h3">I'm Glen</h3>
-                    <Typewriter data="test data" />
-                    <Typewriter data="other test data" />
+                    <Typewriter data={{First: "Glen", Last: "Burchfield"}} />
+                    <Typewriter data={{Profession: "Software Engineer", Expert: "Node.js"}} />
                 </div>
             </section>
             <section className="timeline">
